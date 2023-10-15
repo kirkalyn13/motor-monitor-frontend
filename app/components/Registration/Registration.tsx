539c7fa35cd45f6e77374e5f6a07955f3f425e53
Kirk Santos <kirkalyn13@gmail.com>
2023-10-15 15:05:47 +0800

    feat: add useFirebaseAuth hook

--- app/components/Registration/Registration.tsx
+++ app/components/Registration/Registration.tsx
@@ -1,58 +1,19 @@
 'use client';
 import { useState, useEffect } from "react";
-import {
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  onAuthStateChanged,
-  signOut,
-} from "firebase/auth";
-// import "./Registration.css";
-import { auth } from "@/firebase-config";
+import useFirebaseAuth from "@/app/hooks/useFirebaseAuth";
 
 
 function Registration() {
-  const [registerEmail, setRegisterEmail] = useState("");
-  const [registerPassword, setRegisterPassword] = useState("");
-  const [loginEmail, setLoginEmail] = useState("");
-  const [loginPassword, setLoginPassword] = useState("");
-
-  const [user, setUser] = useState<any>({});
-
-  useEffect(() => {
-      onAuthStateChanged(auth, (currentUser: any) => {
-        setUser(currentUser);
-      });
-  },[])
-
-  const register = async () => {
-    try {
-      const user = await createUserWithEmailAndPassword(
-        auth,
-        registerEmail,
-        registerPassword
-      );
-      console.log(user);
-    } catch (error: any) {
-      console.error(error.message);
-    }
-  };
-
-  const login = async () => {
-    try {
-      const user = await signInWithEmailAndPassword(
-        auth,
-        loginEmail,
-        loginPassword
-      );
-      console.log(user);
-    } catch (error: any) {
-      console.error(error.message);
-    }
-  };
-
-  const logout = async () => {
-    await signOut(auth);
-  };
+  const {
+    register, 
+        login, 
+        logout, 
+        setRegisterEmail, 
+        setRegisterPassword, 
+        setLoginEmail, 
+        setLoginPassword, 
+        user
+  } = useFirebaseAuth()
 
   return (
     <div className="Registration">
---
+++ app/hooks/useFirebaseAuth.tsx
@@ -0,0 +1,66 @@
+import { useState, useEffect } from 'react'
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+} from "firebase/auth";
+// import "./Registration.css";
+import { auth } from "@/firebase-config";
+
+const useFirebaseAuth = () => {
+    const [registerEmail, setRegisterEmail] = useState("");
+    const [registerPassword, setRegisterPassword] = useState("");
+    const [loginEmail, setLoginEmail] = useState("");
+    const [loginPassword, setLoginPassword] = useState("");
+    const [user, setUser] = useState<any>({});
+
+    useEffect(() => {
+        onAuthStateChanged(auth, (currentUser: any) => {
+          setUser(currentUser);
+        });
+    },[])
+
+    const register = async () => {
+        try {
+          const user = await createUserWithEmailAndPassword(
+            auth,
+            registerEmail,
+            registerPassword
+          );
+          console.log(user);
+        } catch (error: any) {
+          console.error(error.message);
+        }
+      };
+    
+      const login = async () => {
+        try {
+          const user = await signInWithEmailAndPassword(
+            auth,
+            loginEmail,
+            loginPassword
+          );
+          console.log(user);
+        } catch (error: any) {
+          console.error(error.message);
+        }
+      };
+    
+      const logout = async () => {
+        await signOut(auth);
+      };
+
+      return {
+        register, 
+        login, 
+        logout, 
+        setRegisterEmail, 
+        setRegisterPassword, 
+        setLoginEmail, 
+        setLoginPassword, 
+        user
+    }
+}
+
+export default useFirebaseAuth
