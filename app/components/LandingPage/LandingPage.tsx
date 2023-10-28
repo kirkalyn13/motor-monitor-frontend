cfd691211afbffd124c6237b6fae0f74b30e99cc
Kirk Santos <kirkalyn13@gmail.com>
2023-10-28 22:38:23 +0800

    style: add fade in on landing page

--- app/components/LandingPage/LandingPage.tsx
+++ app/components/LandingPage/LandingPage.tsx
@@ -7,7 +7,8 @@ const LandingPage = () => {
       className={`h-screen flex flex-col items-center justify-center
         bg-[url('../public/hero.avif')] bg-cover bg-center`}>
       <div className="md:w-1/2 flex flex-col items-center justify-center
-        px-4 py-8 md:m-4 border border-0 md:rounded-lg bg-black bg-opacity-50">
+        px-4 py-8 md:m-4 border border-0 md:rounded-lg bg-black bg-opacity-50
+        opacity-0 animate-fade-in">
         <h1 className="text-4xl my-4 font-bold text-center text-white">
           Empower Your Operations with Wireless Three-Phase Induction Motor Monitoring
         </h1>
@@ -18,15 +19,19 @@ const LandingPage = () => {
       </div>
       <div className="flex flex-col md:flex-row my-8">
         <Link 
-          className="px-16 py-4 mx-4 my-2 text-white text-center border border-0 rounded-md bg-amber-500 bg-opacity-50
-            hover:bg-amber-300 hover:font-bold hover:text-black"
+          className="px-16 py-4 mx-4 my-2 text-white text-center 
+            border border-0 rounded-md bg-amber-500 bg-opacity-50
+            hover:bg-amber-300 hover:font-bold hover:text-black
+            opacity-0 animate-fade-in"
           href="/register" 
           scroll={false}>
           SIGN UP
         </Link>
         <Link 
-          className="px-16 py-4 mx-4 my-2 text-white text-center border border-0 rounded-md bg-black bg-opacity-50
-            hover:bg-gray-500 hover:font-bold"
+          className="px-16 py-4 mx-4 my-2 text-white text-center 
+            border border-0 rounded-md bg-black bg-opacity-50
+            hover:bg-gray-500 hover:font-bold
+            opacity-0 animate-fade-in"
           href="/login" 
           scroll={false}>
           LOGIN
