8db06b4c40fa6ebf644471f6d1181d0cbe5b82d6
Kirk Santos <kirkalyn13@gmail.com>
2023-10-28 22:28:50 +0800

    feat: add reusable button component

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className="bg-blue-600 p-4 text-white fixed bottom-0 w-full">
+    <footer className="bg-slate-800 p-4 text-white bottom-0 w-full">
         <div className="container mx-auto text-center">
             <p>&copy; 2023 Motor Monitor.</p>
         </div>
--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -5,6 +5,7 @@ import { useRouter } from 'next/navigation'
 import { useState } from "react"
 import { BiMenu } from 'react-icons/bi'
 import { LOGO_SRC } from "@/app/utils/src"
+import Button from "../Button/Button"
 
 const Navbar = () => {
     const { authenticate, user } = useFirebaseAuth()
@@ -20,7 +21,7 @@ const Navbar = () => {
     }
 
   return (
-    <nav className="bg-blue-600 p-4">
+    <nav className="w-screen fixed bg-blue-600 p-4">
         <div className="container mx-auto flex flex-wrap justify-between items-center">
             <div className="flex items-center">
                 <Image 
@@ -49,12 +50,13 @@ const Navbar = () => {
             </ul>
 
             <div className="hidden md:flex align-center items-center">
-                <span className="text-white me-4">{user?.email}</span>
-                <button 
-                    className="hidden md:block bg-white text-blue-500 px-4 py-2 rounded-lg hover:bg-blue-100 hover:text-blue-500 focus:outline-none"
+                <span className="text-white text-md me-4">{user?.email}</span>
+                <Button text="Logout" handleOnClick={() => handleLogout()} />
+                {/* <button 
+                    className="hidden md:block bg-amber-500 px-4 py-2 rounded-lg hover:bg-amber-600 focus:outline-none"
                     onClick={() => handleLogout()}>
                     Logout
-                </button>
+                </button> */}
             </div>
         </div>
 
