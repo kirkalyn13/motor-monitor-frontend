8dd95931dfdc1e0ae345b9ec41b3df01f6a0b124
Kirk Santos <kirkalyn13@gmail.com>
2023-11-18 22:31:16 +0800

    feat: improve dashboard load logic

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -12,8 +12,8 @@ const DashboardLayout = ({children}: {children: React.ReactNode}) => {
     const router = useRouter()
 
     useEffect(() => {
-        setTimeout(() => {setInitialLoad(false)}, 2000)
         if (!user) router.push("/")
+        else setInitialLoad(false)
     },[router, user])
 
   return (
