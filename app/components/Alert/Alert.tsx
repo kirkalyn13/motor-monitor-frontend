2873af06d9e0d1126b62da32ece8f66c98179cee
Kirk Santos <kirkalyn13@gmail.com>
2024-03-21 19:58:28 +0800

    feat: alert initial commit

---
+++ app/components/Alert/Alert.tsx
@@ -0,0 +1,34 @@
+import { BiX } from "react-icons/bi"
+import Button from "../Button/Button"
+import { FiAlertCircle } from "react-icons/fi"
+
+interface AlertProps {
+    message: string
+    closeModal: Function
+    severity: "warning" | "critical"
+}
+
+const Alert = ({ message, closeModal, severity = "warning" }:  AlertProps) => {
+    return (
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-80">
+          <div className={`w-screen md:w-1/4 h-auto p-6 
+            rounded-md shadow-md ${severity === "warning" ? "bg-amber-500" : "bg-red-500"}`}>
+            <div className="flex flex-row justify-between align-center mb-4">
+                <div className="flex justify-start align-center">
+                    <FiAlertCircle className="text-3xl me-2"/>
+                    <h2 className="text-2xl font-bold">{ severity === "warning" ? "WARNING" : "CRITICAL"}</h2>
+                </div>
+                <BiX className="text-4xl" onClick={() => closeModal()}/>
+            </div>
+            <p className="m-4 text-start">{message}</p>
+            <Button 
+                text="Close" 
+                color={severity === "warning" ? "bg-amber-700" : "bg-red-700"}
+                handleOnClick={() => closeModal()}
+                />
+          </div>
+        </div>
+      )
+}
+
+export default Alert
--- app/components/Summary/Alarms/Alarms.tsx
+++ app/components/Summary/Alarms/Alarms.tsx
@@ -9,6 +9,7 @@ import { useSearchParams } from 'next/navigation'
 import { jsonToCsvWriter } from '@/app/utils/writer'
 import { getCurrentTimestampString } from '@/app/utils/helpers'
 import useMinuteListener from '@/app/hooks/useMinuteListener'
+import Alert from '../../Alert/Alert'
 
 interface AlarmsProps {
   unitID: string;
@@ -24,50 +25,66 @@ const CSV_HEADERS = {
 }
 
 const Alarms = ({unitID, ratedVoltage, ratedCurrent, maxTemperature}: AlarmsProps) => {
-  const [ alarms, setAlarms ] = useState<Alarm[]>([])
-  const searchParams = useSearchParams()
-  const period = searchParams.get("period") ?? "15"
-  const { currentMinute } = useMinuteListener()
+    const [ alarms, setAlarms ] = useState<Alarm[]>([])
+    const [ showModal, setShowModal ] = useState(false)
+    const searchParams = useSearchParams()
+    const period = searchParams.get("period") ?? "15"
+    const { currentMinute } = useMinuteListener()
 
-  const extractData = async (): Promise<void> => {
-    downloadAlarmsHistory(unitID, ratedVoltage, ratedCurrent, maxTemperature, period)
-      .then((res: any) => {
-        const filename = `alarms-history-${getCurrentTimestampString()}.csv`
-        const logs = [ CSV_HEADERS, ...res ]
-        jsonToCsvWriter(logs, filename)
-      }).catch((err) => {
-        console.error(err)
-      })
-  }
+    const extractData = async (): Promise<void> => {
+      downloadAlarmsHistory(unitID, ratedVoltage, ratedCurrent, maxTemperature, period)
+        .then((res: any) => {
+          const filename = `alarms-history-${getCurrentTimestampString()}.csv`
+          const logs = [ CSV_HEADERS, ...res ]
+          jsonToCsvWriter(logs, filename)
+        }).catch((err) => {
+          console.error(err)
+        })
+    }
+
+    const showAlert = (): void => {
+      if (alarms.some((alarm: Alarm) => alarm.status !== "null")) setShowModal(true)
+    }
+
+    const closeAlert = (): void => {
+      setShowModal(true)
+    }
 
     useEffect(() => {
         getAlarms(unitID, ratedVoltage, ratedCurrent, maxTemperature)
           .then((res) => setAlarms(res))
+          .then(() => showAlert())
     },[currentMinute, maxTemperature, ratedCurrent, ratedVoltage, unitID])
 
   return (
-    <div className="w-screen md:w-1/3">
-      <div className="w-full flex justify-center align-center">
-        <h3 className="px-4 py-2">Alarms</h3>
-        <BiDownload 
-          title="Extract Alarms History"
-          className="text-3xl mt-1 py-1 hover:text-amber-500"
-          onClick={() => extractData()}
-          />
+    <>
+      { !showModal && <Alert 
+        message="Test Alert" 
+        closeModal={() => closeAlert()}/>
+      }
+      <div className="w-screen md:w-1/3">
+        <div className="w-full flex justify-center align-center">
+          <h3 className="px-4 py-2">Alarms</h3>
+          <BiDownload 
+            title="Extract Alarms History"
+            className="text-3xl mt-1 py-1 hover:text-amber-500"
+            onClick={() => extractData()}
+            />
+        </div>
+        <ul className="my-4 space-y-2 text-left">
+          { alarms ?
+            alarms.map((alarm: Alarm) => (
+              <li 
+                className={`ms-8 md:ms-32 ${getStatusTextColor(alarm.status, false)}`} 
+                key={alarm.alarm}>
+                  {alarm.alarm}
+              </li>
+              )
+            ) : null
+          }
+        </ul>
       </div>
-      <ul className="my-4 space-y-2 text-left">
-        { alarms ?
-          alarms.map((alarm: Alarm) => (
-            <li 
-              className={`ms-8 md:ms-32 ${getStatusTextColor(alarm.status, false)}`} 
-              key={alarm.alarm}>
-                {alarm.alarm}
-            </li>
-            )
-          ) : null
-         }
-      </ul>
-    </div>
+    </>
   )
 }
 
